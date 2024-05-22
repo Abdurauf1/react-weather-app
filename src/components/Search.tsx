ebe1aa28965bb43f8ddbf783a66f2baceb32f2ee
Abdurauf <aabdurauf004@gmail.com>
2024-05-23 03:00:03 +0500

    update

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,16 +1,42 @@
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
+import { GEO_API_OPTIONS, GEO_API_URL } from "../api";
 
-const Search = () => {
+const Search = ({ onSearchChange }: any) => {
   const [search, setSearch] = useState(null);
 
-  const loadOptions = (inputValue: string) => {
-    
-  }
+  const loadOptions = (inputValue: any) => {
+    return fetch(
+      `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+      GEO_API_OPTIONS
+    )
+      .then(response => response.json())
+      .then(response => {
+        return {
+          options: response.data.map((city: any) => {
+            return {
+              value: `${city.latitude} ${city.longitude}`,
+              label: `${city.name}, ${city.countryCode}`,
+            };
+          }),
+        };
+      });
+  };
+
+  const handleOnChange = (searchData: any) => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+  };
 
   return (
     <>
-      <AsyncPaginate placeholder="Search city" debounceTimeout={600} value={search} />
+      <AsyncPaginate
+        placeholder="Search city"
+        debounceTimeout={600}
+        value={search}
+        onChange={handleOnChange}
+        loadOptions={loadOptions}
+      />
     </>
   );
 };
